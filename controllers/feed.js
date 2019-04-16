71879ff0f0b7db3ce08796954502394e8139c59b
= <emanuel.alvesc@gmail.com>
2019-04-16 20:51:49 -0300

    Adding validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,27 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
                 title: 'First Post',
                 content: 'This is the first post!',
-                imageUrl: 'images/duck.jpg' 
+                imageUrl: 'images/duck.jpg'
             }
         ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(422).json(
+            {
+                message: 'Validation failed!',
+                errors: error.array()
+            });
+    }
+
     const title = req.body.title;
     const content = req.body.content;
     // Create post in db
